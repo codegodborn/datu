f4a96035f38946fb71031e1351e8c052422e66ec
codegodborn <98381851+codegodborn@users.noreply.github.com>
2025-01-04 01:09:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||小思|我们|在一起吧|❤|#rectangle|');
+      S.UI.simulate('|#countdown 3||小思|很高兴认识你|❤|#rectangle|');
     }
 
     S.Drawing.loop(function () {
